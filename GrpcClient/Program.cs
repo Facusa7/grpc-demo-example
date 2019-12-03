a1c8b864ea5833e93e34c4cd38ccf4daf65e10ee
Facundo Sa <facundo.sa@web.com>
2019-12-03 15:48:06 -0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Facundo" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //Here the greeter server is not being instantiated. The object is being created through the channel.
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message); //This is what the server responds 
+
+            Console.ReadLine();
+        }
+    }
+}
